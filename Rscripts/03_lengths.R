c65efcbb8dc76ba20bde34acba4556b9d3497be9
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-12-17 12:09:29 -0800

    adding length script

---
+++ Rscripts/03_lengths.R
@@ -0,0 +1,12 @@
+#### Daph TSR lengths
+
+
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(stringr)
+
+# read in data ------------------------------------------------------------
+
+lengths <- read_csv("data-raw/daph-tsr-lengths.csv")
