790a84762b92fba0c73abbfc5fe4fbd12fbb2867
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-12-17 12:54:48 -0800

    lengths script plots

--- Rscripts/03_lengths.R
+++ Rscripts/03_lengths.R
@@ -6,7 +6,22 @@
 
 library(tidyverse)
 library(stringr)
+library(lubridate)
+library(plotrix)
 
 # read in data ------------------------------------------------------------
 
-lengths <- read_csv("data-raw/daph-tsr-lengths.csv")
+lengths_raw <- read_csv("data-raw/daph-tsr-lengths.csv")
+
+lengths <- lengths_raw %>% 
+	mutate(date = mdy(date))
+
+lengths %>% 
+	filter(version == 2) %>%
+	filter(temperature > 10) %>% 
+	mutate(length = as.numeric(length)) %>% 
+	filter(length > 1000) %>%
+	group_by(temperature) %>% 
+	summarise_each(funs(mean, std.error), length) %>% 
+	ggplot(aes(x = temperature, y = mean)) + geom_point() +
+	geom_errorbar(aes(ymin = mean - std.error, ymax = mean + std.error), width = 0.1)
