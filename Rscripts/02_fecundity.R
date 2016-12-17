86b249dc1a80ffbf5285c54723469819e419d8e1
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-12-17 12:04:12 -0800

    adding the length data

--- Rscripts/02_fecundity.R
+++ Rscripts/02_fecundity.R
@@ -33,3 +33,18 @@ data_raw %>%
 	ungroup() %>% 
 	do(tidy(lm(mean ~ temperature, data = .), conf.int = TRUE)) %>% View
 
+## what's the relationship between size and fecundity?
+## also plot the growth over time
+
+## need to get body size and fecundity side by side
+### are we missing some fecundity data from the 24s?
+
+v2_babies <- data_raw %>% 
+	filter(grepl("V2", ID)) %>% 
+	separate(ID, into = c("V", "letter"), sep = 2) %>% 
+	unite(unique_id, letter, temperature, sep = "_")
+
+
+all <- left_join(data7, v2_babies, by = "unique_id")
+
+ggplot(data = all, aes(x = max_length, y = individuals, color = factor(temperature))) + geom_point(size = 4)
