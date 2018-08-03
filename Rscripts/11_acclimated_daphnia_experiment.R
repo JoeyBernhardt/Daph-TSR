c5dac4cfc40e4a6e942c6c736e83b2be752abf94
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-08-03 11:55:05 +0200

    plotting data from acclimated daphnia

--- Rscripts/11_acclimated_daphnia_experiment.R
+++ Rscripts/11_acclimated_daphnia_experiment.R
@@ -24,3 +24,19 @@ acc %>%
 	summarise_each(funs(max), size_um) %>% 
 	ggplot(aes(x = temperature, y = size_um)) + geom_point()
 
+
+acc %>% 
+	mutate(mass =  0.00402*((size_um/1000)^2.66)) %>% 
+	filter(stage %in% c("clutch1","clutch2", "clutch3", "clutch4", "clutch5", "clutch6")) %>% 
+	ggplot(aes(x = temperature, y = mass)) + geom_point() + 
+	geom_smooth(method = "lm") +
+	facet_wrap( ~ stage)
+
+### now get the clutch sizes for the acclimated daphnia
+
+acc_clutch <- read_csv("data-raw/acclimated-daphnia-clutches.csv") 
+
+acc_clutch %>% 
+	filter(clutch_number < 7) %>% 
+	ggplot(aes(x = temperature, y = final_baby_count)) + geom_point() +
+	facet_wrap( ~ clutch_number, scales = "free") + geom_smooth()
